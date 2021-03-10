42d6a4aea6de41c36c9e4900641f36c5cabf0d5d
Haess Romani <hromaza@gmail.com>
2021-03-10 17:56:03 -0500

    store bookmarks to localStorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,7 +31,6 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
     // 0) Update results view to mark selected mark result
     resultsView.update(model.getSearchResultsPage());
-    bookmarksView.update(model.state.bookmarks);
     // 1) Loading recipe
     await model.loadRecipe(id);
 
@@ -39,7 +38,8 @@ const controlRecipes = async function () {
 
     recipeView.render(model.state.recipe);
 
-    // console.log(recipe.ingredients);
+    // 3) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
   } catch (err) {
     // console.log(err);
     recipeView.renderError();
@@ -93,7 +93,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
--- src/js/model.js
+++ src/js/model.js
@@ -74,6 +74,10 @@ export const updateServings = function (newServings) {
   state.recipe.servings = newServings;
 };
 
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
 export const addBookmark = function (recipe) {
   // Add bookmark
   state.bookmarks.push(recipe);
@@ -81,6 +85,7 @@ export const addBookmark = function (recipe) {
   if (recipe.id === state.recipe.id) {
     state.recipe.bookmarked = true;
   }
+  persistBookmarks();
 };
 
 export const deleteBookmark = function (id) {
@@ -90,4 +95,18 @@ export const deleteBookmark = function (id) {
   if (id === state.recipe.id) {
     state.recipe.bookmarked = false;
   }
+  persistBookmarks();
 };
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
+
+// clearBookmarks();
