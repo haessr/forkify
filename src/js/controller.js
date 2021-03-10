61f309519036f29419c0f4a787efd0d6ac58c6dd
Haess Romani <hromaza@gmail.com>
2021-03-10 17:08:05 -0500

    Bookmarks implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,11 +2,11 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
-import PaginationView from './views/paginationView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import paginationView from './views/paginationView.js';
 
 // if (module.hot) {
 //   module.hot.accept();
@@ -31,6 +31,7 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
     // 0) Update results view to mark selected mark result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
     // 1) Loading recipe
     await model.loadRecipe(id);
 
@@ -83,10 +84,13 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add or remove a bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
-
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+  // 3) Render the bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
